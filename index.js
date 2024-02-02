6b8c09fe754dec2a910ffc661ead4eac52eb9549
ANAS727189 <144107389+ANAS727189@users.noreply.github.com>
2024-02-02 23:33:41 +0530

    bas ab bahut ho gaya hai last

--- index.js
+++ index.js
@@ -11,9 +11,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+
 
 
 
